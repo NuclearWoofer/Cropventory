e82cfe0b403dd744f22b926e5095a7172bc4ed98
Michael <malopez1@email.neit.edu>
2021-08-18 10:46:05 -0400

    editing header

---
+++ includes/header.php
@@ -0,0 +1,116 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  <title>SE 266 - Michael Lopez</title>
+<style>
+body {
+  font-family: Cambria, Cochin, Georgia, Times, 'Times New Roman', serif;;
+  font-size: "16px;";
+  margin-left: 20px;
+  margin-right: 10px;
+}
+.navbar {
+  overflow: hidden;
+  background-color: #333;
+  font-family: Arial, Helvetica, sans-serif;
+}
+.navbar a {
+  float: left;
+  font-size: 16px;
+  color: white;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+}
+.dropdown {
+  float: left;
+  overflow: hidden;
+}
+.dropdown .dropbtn {
+  cursor: pointer;
+  font-size: 16px;  
+  border: none;
+  outline: none;
+  color: white;
+  padding: 14px 16px;
+  background-color: inherit;
+  font-family: inherit;
+  margin: 0;
+}
+.navbar a:hover, .dropdown:hover .dropbtn, .dropbtn:focus {
+  background-color: red;
+}
+.dropdown-content {
+  display: none;
+  position: absolute;
+  background-color: #f9f9f9;
+  min-width: 160px;
+  box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
+  z-index: 1;
+}
+.dropdown-content a {
+  float: none;
+  color: black;
+  padding: 12px 16px;
+  text-decoration: none;
+  display: block;
+  text-align: left;
+}
+.dropdown-content a:hover {
+  background-color: #ddd;
+}
+.show {
+  display: block;
+}
+li {
+  font-family: "Times New Roman", Times, serif;
+  font-size: "16px;";
+}
+</style>
+</head>
+<body>
+<div class="navbar">
+  
+  <div class="dropdown">
+    <button class="dropbtn" onclick="dropDown()">Assignments
+      <i class="fa fa-caret-down"></i>
+    </button>
+    <div class="dropdown-content" id="myDropdown">
+      <a href="../Week1/fizzBuzz.php">Assignment 1 - Fizz Buzz</a>
+      <a href="../week2/patientIntake.view.php">Assignment 2 - Patient Intake</a>
+      <a href="../week3/index.php">Assignment 3 - ATM</a>
+      <a href="../week4/patientView.php">Adding Patients / Patient Intake</a>
+      <a href="../week4/patientView.php">Deleting / Updating Patient Records</a>
+      <a href="../week4/patientView.php">Patient Metrics</a>
+      <a href="../week4/login.php">Patient Search + Login Page</a>
+      <a href="../week8/data.php">Disney Votes</a>
+      <a href="../week10/login.php">Final Project - Farmers Website</a> 
+      <a href="../week5/carsView.php">Extra Credit 1 - Cars Inventory</a>
+    </div>
+  </div> 
+  <a href="../site/index.php">Home Screen</a>
+  <a href="../site/heroku_resources.php">Heroku Resources</a>
+  <a href="../site/php_resources.php">PHP Resources</a>
+  <a href="../site/git_resources.php">Git Resources</a>
+  <a href="https://github.com/NuclearWoofer/SE266">My GitHub Repo</a>
+  <a href="../week10/login.php">Final Project</a>
+  
+</div>
+<script>
+/* When the user clicks on the button, 
+toggle between hiding and showing the dropdown content */
+function dropDown() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(e) {
+  if (!e.target.matches('.dropbtn')) {
+  var myDropdown = document.getElementById("myDropdown");
+    if (myDropdown.classList.contains('show')) {
+      myDropdown.classList.remove('show');
+    }
+  }
+}
+</script>
