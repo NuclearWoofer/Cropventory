daa817e283698011ad899cff4fcc2776654965ac
Michael <malopez1@email.neit.edu>
2021-09-01 10:36:12 -0400

    tweaking

---
+++ crops.sql
@@ -0,0 +1,14 @@
+
+--below is mostly user input, cropEstHarvest  & cropEstSell would be auto based on cropName
+CREATE TABLE IF NOT EXISTS cropsInventory (
+	cropId INT UNSIGNED AUTO_INCREMENT NOT NULL PRIMARY KEY,
+        cropName VARCHAR(50) DEFAULT NULL, --name chosen on dropdown
+        cropPlanted DATE, --date planted
+        cropEstHarvest DATE, --estimated harvest date
+        cropQty INT, --how many of said plant
+        cropEstSell FLOAT, --estimated sell = qty 
+        
+        
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1;
+
+
