d1e4c8550407e8757a9f55fefbb80ee420603fe7
Michael <malopez1@email.neit.edu>
2021-09-02 09:25:15 -0400

    update crops.sql
    updated DB headers

--- crops.sql
+++ crops.sql
@@ -1,13 +1,13 @@
-
---below is mostly user input, cropEstHarvest  & cropEstSell would be auto based on cropName
-CREATE TABLE IF NOT EXISTS cropsInventory (
+  --below is mostly user input, cropEstHarvest  & cropEstSell would be auto based on cropName
+CREATE TABLE IF NOT EXISTS crops (
 	cropId INT UNSIGNED AUTO_INCREMENT NOT NULL PRIMARY KEY,
         cropName VARCHAR(50) DEFAULT NULL, --name chosen on dropdown
         cropPlanted DATE, --date planted
         cropEstHarvest DATE, --estimated harvest date
         cropQty INT, --how many of said plant
         cropEstSell FLOAT, --estimated sell = qty 
-        
+        category VARCHAR(50),
+        subCategory VARCHAR(50),
         
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1;
 
